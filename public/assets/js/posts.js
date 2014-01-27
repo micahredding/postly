e923365669bf7a4a7fced8c6001545c5b3cf19d9
Micah Redding <micahtredding@gmail.com>
2014-01-26 18:49:53 -0600

    Updating fontawesome and linking h2

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -2,4 +2,7 @@ $ = jQuery;
 $(document).ready(function() {
   $(".post").fitVids();
   $('audio').audioPlayer();
+  $('.stream-posts article').each(function(index){
+  	$(this).find('h2').wrapInner('<a href="' + $(this).data('url') + '"></a>');
+  })
 });
