5a3a07ac82b77a5dd8ec9289dc50707337242f2a
Micah Redding <micahtredding@gmail.com>
2014-02-14 00:20:29 -0600

    Having markdown and yaml alternatives

--- dao/uri_dao.rb
+++ dao/uri_dao.rb
@@ -1,9 +1,16 @@
 require 'open-uri'
 
-class MarkdownDao
+class UriDao
   def load_and_parse(filename)
-  	open(filename) do |f|
-	    f.read
-	  end
+    open(filename) do |f|
+      f.read
+    end
+  end
+end
+
+
+class MarkdownDao < UriDao
+  def load_and_parse(filename)
+    super filename
   end
 end
