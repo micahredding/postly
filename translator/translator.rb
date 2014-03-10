fb213b9b7da75cc64bfbd811ac0e44aa589d59b3
Micah Redding <micahtredding@gmail.com>
2014-03-10 09:42:53 -0500

    adding in requirements

--- translator/translator.rb
+++ translator/translator.rb
@@ -1,3 +1,8 @@
+require "#{Postly::TRANSLATOR_PATH}/meta_dao"
+require "#{Postly::TRANSLATOR_PATH}/post_dao"
+require "#{Postly::TRANSLATOR_PATH}/stream_dao"
+require "#{Postly::MAPPER_PATH}/post_mapper"
+
 class Translator
   def import(source, destination)
     destination.import_to_database source.index
